05a9d7d919417459208e1e6702a9b39f6b53498b
Yunzheng <42thcoder@gmail.com>
2015-05-14 20:57:56 +0800

    done

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 
 //var createMiniHarp = require("mini-harp")
-var createMiniHarp = require("..")
-    , app = createMiniHarp(),
-    minimist = require('minimist'),
-    argv = minimist(process.argv.slice(2));
 
+var miniHarp = require(".."),
+    argv = require('minimist')(process.argv.slice(2));
+
+var root = process.cwd();
+var app = miniHarp(root);
 
 console.log("Starting mini-harp on http://localhost:" + argv.port);
 
-app.use(function(request, response, next){
-    if(request.url === '/current-time'){
-        response.end((new Date()).toISOString());
-    }
-});
-
-app.listen(argv.port || 4000);
-
+app.listen(4000);
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var connect = require('connect');
+var serveStatic = require('serve-static');
 
-module.exports = function(){
-    return connect();
+module.exports = function (path) {
+    var app = connect();
+
+    app
+        .use(function (request, response, next) {
+            if (request.url === '/current-time') {
+                response.end((new Date()).toISOString());
+            }
+            next();
+        })
+        .use(serveStatic(path));
+
+
+    return app;
 };
